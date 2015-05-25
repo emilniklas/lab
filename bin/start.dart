8f884bb953d72a09501d385ac457265acdde3ed5
Emil Persson <emil.n.persson@gmail.com>
2015-05-25 11:11:21 +0200

    ett s

--- bin/start.dart
+++ bin/start.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
-
 main() async {
   var server = await HttpServer.bind('localhost', 1337);
   await for(HttpRequest request in server) {
-    request.response.write('Hello World!');
+    request.response.write('Hello Worlds!');
     request.response.close();
   }
 }
