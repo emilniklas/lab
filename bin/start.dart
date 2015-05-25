aeb594175c7390504047fba2d4545b279b82de13
Emil Persson <emil.n.persson@gmail.com>
2015-05-25 11:13:48 +0200

    logga path

--- bin/start.dart
+++ bin/start.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 main() async {
   var server = await HttpServer.bind('localhost', 1337);
   await for(HttpRequest request in server) {
+    print(request.uri.path);
     if (request.uri.path == '/trigger') throw 'ERROR!';
     request.response.write('Hello Worlds!');
     request.response.close();
