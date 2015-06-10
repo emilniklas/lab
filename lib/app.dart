a031ddfd5f8ff67375aa02eb46fe74d86eaa6a37
Emil Persson <emil.n.persson@gmail.com>
2015-06-10 15:58:51 +0200

    Updates

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 export 'package:bridge/http.dart';
 export 'package:bridge/view.dart';
 export 'package:bridge/tether.dart';
+export 'package:bridge/generators.dart';
 
 export 'framework.dart';
 export 'app/lib.dart';
